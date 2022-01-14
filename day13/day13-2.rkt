7207476a84876f60b5c4f6f4c92b9469f79fe7eb
Stephen Pearson <stephen.pearson@oracle.com>
2022-01-14 10:24:25 +0000

    Refactor day 13

--- day13/day13-2.rkt
+++ day13/day13-2.rkt
@@ -5,19 +5,19 @@
 (require "../common/read_files.rkt")
 
 (define points
-  (make-hash (map (lambda (l) (cons l #t))
-    (map (lambda (l) (map string->number l))
-      (filter (lambda (l) (= (length l) 2))
-        (map (lambda (l) (string-split l ",")) (read-file-lines "input")))))))
+  (make-hash (map (λ (l) (cons l #t))
+    (map (λ (l) (map string->number l))
+      (filter (λ (l) (= (length l) 2))
+        (map (λ (l) (string-split l ",")) (read-file-lines "input")))))))
 
 (define foldings
-  (map (lambda (l) (let ([v (string->number (cadr (string-split l "=")))])
+  (map (λ (l) (let ([v (string->number (cadr (string-split l "=")))])
     (if (equal? (string-ref l 11) #\x) (cons v #f) (cons #f v))))
-      (filter (lambda (l) (and (> (string-length l) 0) (equal? (string-ref l 0) #\f)))
+      (filter (λ (l) (and (> (string-length l) 0) (equal? (string-ref l 0) #\f)))
         (read-file-lines "input"))))
 
 (define (fold points f)
-  (make-hash (map (lambda (l) (cons l #t))
+  (make-hash (map (λ (l) (cons l #t))
     (for/list ([p (hash-keys points)])
       (if (car f)
         (if (> (car p) (car f)) (cons (- (car f) (- (car p) (car f))) (cdr p)) p)
@@ -29,7 +29,7 @@
 
 (define (plot points)
   (for ([y (in-range 6)])
-    (for ([x (in-range 50)])
+    (for ([x (in-range 40)])
       (printf "~a" (if (hash-ref points (list x y) #f) "#" ".")))
     (printf "\n")))
 
